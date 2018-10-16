17cab010fc130839792fea0037c98d772d8200b4
berdjugin-jf <jean-francois.berdjugin@univ-nantes.fr>
2018-10-16 15:13:54 +0200

    GestionInscriptions

---
+++ Asso/domaine/src/test/java/fr/univ/nantes/iut/asso/domaine/GestionInscriptions.java
@@ -0,0 +1,15 @@
+package fr.univ.nantes.iut.asso.domaine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GestionInscriptions {
+
+	@Test
+	void test() {
+		
+		fail("Not yet implemented");
+	}
+
+}
